3ef83e32a2f088c6af002134a045a1e97a36e4ff
Kairen1997 <kairen.deiwien@gmail.com>
2025-08-13 10:14:55 +0800

    updating my authentication according to my design & add migrations for full name

--- lib/umrahly_web/live/user_login_live.ex
+++ lib/umrahly_web/live/user_login_live.ex
@@ -3,6 +3,7 @@ defmodule UmrahlyWeb.UserLoginLive do
 
   def render(assigns) do
     ~H"""
+<<<<<<< Updated upstream
     <div class="mx-auto max-w-sm">
       <.header class="text-center">
         Log in to account
@@ -14,11 +15,29 @@ defmodule UmrahlyWeb.UserLoginLive do
           for an account now.
         </:subtitle>
       </.header>
+=======
+    <div class="w-[542px] h-[542px] flex flex-col items-center justify-center bg-amber-200 shadow-lg rounded-lg">
+        <div class="max-w-md w-full bg-[#EAD4AB] rounded-lg p-8 shadow-lg">
+          <h2 class="text-center text-xl font-semibold mb-6">Login to your account</h2>
+          <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore" class = "space-y-6">
+            <div >
+              <label for="email" class="block mb-1 text-sm font-medium text-gray-900">Email</label>
+              <.input
+                field={@form[:email]}
+                id="email"
+                type="email"
+                placeholder="Enter your email"
+                required
+                class="w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-teal-600"
+              />
+            </div>
+>>>>>>> Stashed changes
 
       <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore">
         <.input field={@form[:email]} type="email" label="Email" required />
         <.input field={@form[:password]} type="password" label="Password" required />
 
+<<<<<<< Updated upstream
         <:actions>
           <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
           <.link href={~p"/users/reset_password"} class="text-sm font-semibold">
@@ -31,6 +50,19 @@ defmodule UmrahlyWeb.UserLoginLive do
           </.button>
         </:actions>
       </.simple_form>
+=======
+            <div>
+              <.button phx-disable-with="Logging in..."  class="w-full bg-[#00897B] text-white py-3 rounded-md hover:bg-[#00796B] font-semibold">
+                Login
+              </.button>
+            </div>
+          </.simple_form>
+          <p class="mt-4 text-center text-sm text-gray-700">
+            Don't have an account?
+            <.link navigate={~p"/users/register"} class="text-teal-700 hover:underline">Register</.link>
+          </p>
+        </div>
+>>>>>>> Stashed changes
     </div>
     """
   end

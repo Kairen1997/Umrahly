0c0806b58b87a23f2a11298e866d2e4b20611818
Kairen1997 <kairen.deiwien@gmail.com>
2025-08-13 10:35:57 +0800

    update authentication

--- lib/umrahly_web/live/user_login_live.ex
+++ lib/umrahly_web/live/user_login_live.ex
@@ -3,66 +3,46 @@ defmodule UmrahlyWeb.UserLoginLive do
 
   def render(assigns) do
     ~H"""
-<<<<<<< Updated upstream
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">
-        Log in to account
-        <:subtitle>
-          Don't have an account?
-          <.link navigate={~p"/users/register"} class="font-semibold text-brand hover:underline">
-            Sign up
-          </.link>
-          for an account now.
-        </:subtitle>
-      </.header>
-=======
     <div class="w-[542px] h-[542px] flex flex-col items-center justify-center bg-amber-200 shadow-lg rounded-lg">
-        <div class="max-w-md w-full bg-[#EAD4AB] rounded-lg p-8 shadow-lg">
-          <h2 class="text-center text-xl font-semibold mb-6">Login to your account</h2>
-          <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore" class = "space-y-6">
-            <div >
-              <label for="email" class="block mb-1 text-sm font-medium text-gray-900">Email</label>
-              <.input
-                field={@form[:email]}
-                id="email"
-                type="email"
-                placeholder="Enter your email"
-                required
-                class="w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-teal-600"
-              />
-            </div>
->>>>>>> Stashed changes
+      <div class="max-w-md w-full bg-[#EAD4AB] rounded-lg p-8 shadow-lg">
+        <h2 class="text-center text-xl font-semibold mb-6">Login to your account</h2>
+        <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore" class="space-y-6">
+          <div>
+            <label for="email" class="block mb-1 text-sm font-medium text-gray-900">Email</label>
+            <.input
+              field={@form[:email]}
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+              required
+              class="w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-teal-600"
+            />
+          </div>
 
-      <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore">
-        <.input field={@form[:email]} type="email" label="Email" required />
-        <.input field={@form[:password]} type="password" label="Password" required />
+          <div>
+            <label for="password" class="block mb-1 text-sm font-medium text-gray-900">Password</label>
+            <.input
+              field={@form[:password]}
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+              required
+              class="w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-teal-600"
+            />
+          </div>
 
-<<<<<<< Updated upstream
-        <:actions>
-          <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
-          <.link href={~p"/users/reset_password"} class="text-sm font-semibold">
-            Forgot your password?
-          </.link>
-        </:actions>
-        <:actions>
-          <.button phx-disable-with="Logging in..." class="w-full">
-            Log in <span aria-hidden="true">→</span>
-          </.button>
-        </:actions>
-      </.simple_form>
-=======
-            <div>
-              <.button phx-disable-with="Logging in..."  class="w-full bg-[#00897B] text-white py-3 rounded-md hover:bg-[#00796B] font-semibold">
-                Login
-              </.button>
-            </div>
-          </.simple_form>
-          <p class="mt-4 text-center text-sm text-gray-700">
-            Don't have an account?
-            <.link navigate={~p"/users/register"} class="text-teal-700 hover:underline">Register</.link>
-          </p>
-        </div>
->>>>>>> Stashed changes
+          <:actions>
+            <.button phx-disable-with="Logging in..." class="w-full bg-[#00897B] text-white py-3 rounded-md hover:bg-[#00796B] font-semibold">
+              Login
+            </.button>
+          </:actions>
+        </.simple_form>
+
+        <p class="mt-4 text-center text-sm text-gray-700">
+          Don't have an account?
+          <.link navigate={~p"/users/register"} class="text-teal-700 hover:underline">Register</.link>
+        </p>
+      </div>
     </div>
     """
   end

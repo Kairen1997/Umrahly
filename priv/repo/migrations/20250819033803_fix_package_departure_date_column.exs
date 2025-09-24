0caae1141368c17281a9a113ca993b8911344b8c
Kairen1997 <kairen.deiwien@gmail.com>
2025-09-24 20:07:43 +0800

    update

--- priv/repo/migrations/20250819033803_fix_package_departure_date_column.exs
+++ priv/repo/migrations/20250819033803_fix_package_departure_date_column.exs
@@ -1,7 +1,34 @@
 defmodule Umrahly.Repo.Migrations.FixPackageDepartureDateColumn do
   use Ecto.Migration
 
-  def change do
-    execute "ALTER TABLE packages RENAME COLUMN depature_date TO departure_date"
+  def up do
+    execute """
+    DO $$
+    BEGIN
+      IF EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name = 'packages' AND column_name = 'depature_date'
+      ) THEN
+        ALTER TABLE packages RENAME COLUMN depature_date TO departure_date;
+      END IF;
+    END$$;
+    """
+  end
+
+  def down do
+    execute """
+    DO $$
+    BEGIN
+      IF EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name = 'packages' AND column_name = 'departure_date'
+      ) AND NOT EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name = 'packages' AND column_name = 'depature_date'
+      ) THEN
+        ALTER TABLE packages RENAME COLUMN departure_date TO depature_date;
+      END IF;
+    END$$;
+    """
   end
 end
